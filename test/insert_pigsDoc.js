c73e65dc9d9f2c2d177dbbf793aa5f219c228739
wjkim81 <onejean81@gmail.com>
2018-01-24 17:46:03 +0900

    Add scheduling for load pigs via open-api

--- test/insert_pigsDoc.js
+++ test/insert_pigsDoc.js
@@ -5,7 +5,7 @@ var pigsDocStr = fs.readFileSync('pigsDoc.json');
 var pigsDoc = JSON.parse(pigsDocStr);
 console.log(pigsDoc);
 
-db.insert(pigsDoc, '_design/pigsDoc', (err, insertResult) => {
+db.update(pigsDoc, '_design/pigsDoc', (err, insertResult) => {
   if (err) console.log(`[error] ${err}`);
 
   console.log(insertResult);
